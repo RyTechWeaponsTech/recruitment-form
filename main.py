8071b9ab877911059710419feaa56240ebb138a5
archivedghost001 <aryamuhammadkamil@gmail.com>
2024-04-15 14:08:13 +0700

    real

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# URI = Universal Resource Locator
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.sqlite3"
+app.config["SECRET_KEY"] = "RYTECH117"
+db = SQLAlchemy(app)
+
+# create a table
+
+
+class students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(100))
+    Age = db.Column(db.Integer)
+    Birthday = db.Column(db.String(100))
+    Gender = db.Column(db.String(10))
+
+    def __init__(self, name, age, birthday, gender):
+        self.Name = name
+        self.Age = age
+        self.Birthday = birthday
+        self.Gender = gender
+
+
+@app.route("/", methods=["GET", "POST"])
+def add_recruit():
+    if request.method == "POST":
+        name = request.form["name"]
+        age = request.form["age"]
+        birthday = request.form["birthday"]
+        gender = request.form["gender"]
+        recruit = students(name, age, birthday, gender)
+        db.session.add(recruit)
+        db.session.commit()
+        return redirect(url_for("successfully"))
+    return render_template("add_recruit.html")
+
+
+@app.route("/successfully")
+def successfully():
+    return render_template("successfully.html")
+
+
+@app.route("/show_detail")
+def show_detail():
+    # fetch all recruits records from the db
+    all_recruits = students.query.all()
+    return render_template("show_detail.html", students=all_recruits)
+
+
+@app.route("/clea_database", methods=["GET", "POST"])
+def clear_database():
+    if request.method == "POST":
+        # Deletes all data
+        db.session.query(students).delete()
+        db.session.commit()
+
+        # redirect to the show details
+        return redirect(url_for("successfully"))
+    # render a template for confirmation
+    return render_template("clear_databse.html")
+
+
+if __name__ == "__name__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port="8081")
